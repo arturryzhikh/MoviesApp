4edb060541ccf5f6b3da30fb9da0bcb31d045701
Artur <ryzhiha@gmail>
2020-08-28 15:16:42 +0300

    change SearchMovieViewController bcgcolor

--- MoviesApp/Views/Movies Views/SearchMovieViewController.swift
+++ MoviesApp/Views/Movies Views/SearchMovieViewController.swift
@@ -22,6 +22,7 @@ class SearchMovieViewController: UICollectionViewController{
   }
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
+    view.backgroundColor = #colorLiteral(red: 0.05098039216, green: 0.1450980392, blue: 0.2470588235, alpha: 1)
     setupNavigationBar()
     searchBar = makeSearchBar()
     setupCollectionView()
